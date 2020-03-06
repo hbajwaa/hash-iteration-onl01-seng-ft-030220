5fe1f9aa532a9161dec88626348ae919f6fd1227
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-06 23:20:09 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,5 +8,6 @@
 
 def select_winner(passengers)
   passengers.each do |suite, name|
-    return name if suite == :suite_a and name.start_with("A")
+    return name if suite == :suite_a && name.start_with?("A")
+  end
 end
